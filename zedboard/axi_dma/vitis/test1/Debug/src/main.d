4945372c80be0de8c39bea9a64f1588b411a67e3
wa <1954729948@qq.com>
2020-10-30 21:42:13 +0800

    axi dma

---
+++ zedboard/axi_dma/vitis/test1/Debug/src/main.d
@@ -0,0 +1,70 @@
+src/main.o src/main.o: ../src/main.c \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xparameters.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xparameters_ps.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xaxidma.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xaxidma_bdring.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xstatus.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_types.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_assert.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xaxidma_bd.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xaxidma_hw.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_io.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_printf.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xparameters.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/bspconfig.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xpseudo_asm.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xreg_cortexa9.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xpseudo_asm_gcc.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xdebug.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_cache.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xscugic.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xscugic_hw.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_exception.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_exception.h \
+ E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_printf.h
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xparameters.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xparameters_ps.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xaxidma.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xaxidma_bdring.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xstatus.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_types.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_assert.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xaxidma_bd.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xaxidma_hw.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_io.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_printf.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xparameters.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/bspconfig.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xpseudo_asm.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xreg_cortexa9.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xpseudo_asm_gcc.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xdebug.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_cache.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xscugic.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xscugic_hw.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_exception.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_exception.h:
+
+E:/fpga_proj/ps/axi_dma/vitis/axi_dma/export/axi_dma/sw/axi_dma/standalone_domain/bspinclude/include/xil_printf.h:
