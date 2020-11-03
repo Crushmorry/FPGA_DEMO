55a92887f50d105629e7235448871a2fbc1beaf7
wa <1954729948@qq.com>
2020-11-03 11:38:41 +0800

    update

--- zedboard/hdmi_adv7511/hdmi.srcs/sources_1/new/create_color.v
+++ zedboard/hdmi_adv7511/hdmi.srcs/sources_1/new/create_color.v
@@ -31,4 +31,18 @@ output [7:0] rgb_r,
 output [7:0] rgb_g,
 output [7:0] rgb_b
     );
+    
+parameter H_TOTAL = 12'd1650;    
+parameter H_ACTIVE = 12'd1280;
+parameter H_FP = 12'd110;
+parameter H_SYN = 12'd40;
+parameter H_BP = 12'd220;
+
+parameter V_TOTAL = 12'd750; 
+parameter V_ACTIVE = 12'd720;
+parameter V_FP = 12'd5;
+parameter V_SYN = 12'd5;
+parameter V_BP = 12'd20;
+
+
 endmodule
