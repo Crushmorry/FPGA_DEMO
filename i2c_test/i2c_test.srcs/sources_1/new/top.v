b1b8bccba92411686477e1914f4edecc8870e9ed
wa <1954729948@qq.com>
2020-11-12 20:50:09 +0800

    update

--- i2c_test/i2c_test.srcs/sources_1/new/top.v
+++ i2c_test/i2c_test.srcs/sources_1/new/top.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2020/11/10 16:44:04
+// Create Date: 2020/11/12 20:47:11
 // Design Name: 
 // Module Name: top
 // Project Name: 
@@ -23,4 +23,35 @@
 module top(
 
     );
+    
+    
+    axi_iic_0 your_instance_name (
+  .s_axi_aclk(s_axi_aclk),        // input wire s_axi_aclk
+  .s_axi_aresetn(s_axi_aresetn),  // input wire s_axi_aresetn
+  .iic2intc_irpt(iic2intc_irpt),  // output wire iic2intc_irpt
+  .s_axi_awaddr(s_axi_awaddr),    // input wire [8 : 0] s_axi_awaddr
+  .s_axi_awvalid(s_axi_awvalid),  // input wire s_axi_awvalid
+  .s_axi_awready(s_axi_awready),  // output wire s_axi_awready
+  .s_axi_wdata(s_axi_wdata),      // input wire [31 : 0] s_axi_wdata
+  .s_axi_wstrb(s_axi_wstrb),      // input wire [3 : 0] s_axi_wstrb
+  .s_axi_wvalid(s_axi_wvalid),    // input wire s_axi_wvalid
+  .s_axi_wready(s_axi_wready),    // output wire s_axi_wready
+  .s_axi_bresp(s_axi_bresp),      // output wire [1 : 0] s_axi_bresp
+  .s_axi_bvalid(s_axi_bvalid),    // output wire s_axi_bvalid
+  .s_axi_bready(s_axi_bready),    // input wire s_axi_bready
+  .s_axi_araddr(s_axi_araddr),    // input wire [8 : 0] s_axi_araddr
+  .s_axi_arvalid(s_axi_arvalid),  // input wire s_axi_arvalid
+  .s_axi_arready(s_axi_arready),  // output wire s_axi_arready
+  .s_axi_rdata(s_axi_rdata),      // output wire [31 : 0] s_axi_rdata
+  .s_axi_rresp(s_axi_rresp),      // output wire [1 : 0] s_axi_rresp
+  .s_axi_rvalid(s_axi_rvalid),    // output wire s_axi_rvalid
+  .s_axi_rready(s_axi_rready),    // input wire s_axi_rready
+  .sda_i(sda_i),                  // input wire sda_i
+  .sda_o(sda_o),                  // output wire sda_o
+  .sda_t(sda_t),                  // output wire sda_t
+  .scl_i(scl_i),                  // input wire scl_i
+  .scl_o(scl_o),                  // output wire scl_o
+  .scl_t(scl_t),                  // output wire scl_t
+  .gpo(gpo)                      // output wire [0 : 0] gpo
+);
 endmodule
